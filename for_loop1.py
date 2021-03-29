21a523de3f2e99b8f916d292917dfea7d3f566ae
enes3253 <80917934+enes3253@users.noreply.github.com>
2021-03-29 13:32:44 +0200

    Add files via upload

---
+++ for_loop1.py
@@ -0,0 +1,24 @@
+for i in range(0,501):
+    print(i)
+
+
+for i in range(5,1000,5):
+    print(i)
+
+for i in range(1,101):
+    if i % 5 :
+        print("Coding")
+    if i % 10 :
+        print("Coding Dojo")    
+    print(i)
+
+for i in range(0,500000):
+    if i%2:
+        print(i)
+
+for i in range(2018, 0, -4):
+    print(i)
+
+for i in range(4, 16 + 2):
+    if i % 4 == 0:
+      print(i)
